18184e96f1b76fefea8464a54dc467489ba8beaf
WindSt <BraveNW148@gmail.com>
2020-12-15 02:14:37 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password ? \n"))
+nr_symbols = int(input(f"How many symbols would you like ? \n"))
+nr_numbers = int(input(f"How many numbers would you like ? \n"))
+
+# password = ""
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+#
+# pre_symbols = ""
+# for count in range(1, nr_symbols + 1):
+#     pre_symbols += random.choice(symbols)
+#
+# pre_number = ""
+# for go in range(1, nr_numbers + 1):
+#     pre_number += random.choice(numbers)
+#
+# print(password+pre_symbols+pre_number)
+
+password_list = []
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+pre_symbols = ""
+for count in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+pre_number = ""
+for go in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+new_password = ""
+for word in password_list:
+    new_password += word
+print(new_password)
